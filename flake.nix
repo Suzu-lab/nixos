e3acc29308b4503121763f7d7983319eeddc6a3a
Suzu <sou.suzumi@gmail.com>
2025-10-01 00:50:13 +0000

    base NixOS 25.05 + flakes + HM (host vm)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+ description = "NixOS + Home Manager base setup";
+
+ inputs = {
+  nixpkgs.url = " github:NixOS/nixpkgs/nixos-25.05";
+  home-manager.url = "github:nix-community/home-manager";
+  home-manager.inputs.nixpkgs.follows = "nixpkgs";
+ };
+
+ outputs = { self, nixpkgs, home-manager, ... }:
+  let
+   forSystem = system: pkgs: {
+   };
+  in {
+   nixosConfigurations = { 
+    vm = nixpkgs.lib.nixosSystem {
+     system = "x86_64-linux";
+     modules = [
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+      {
+       home-manager.useGlobalPkgs = true;
+       home-manager.useUserPackages = true;
+       home-manager.users.suzu = {
+        home.stateVersion = "25.05";
+        program.home-manager.enable = true;
+       };
+      }
+     ];
+    };
+   };
+  };
+}
