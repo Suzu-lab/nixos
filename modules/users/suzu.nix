2c80d26dd4f4648c74f320ea428d71a7f624b136
Suzu <sou.suzumi@gmail.com>
2025-10-01 05:50:29 -0300

    Fix fish in suzu.nix

--- modules/users/suzu.nix
+++ modules/users/suzu.nix
@@ -4,11 +4,11 @@
     home.username = "suzu";
     home.homeDirectory = "/home/suzu";
     home.stateVersion = "25.05";
-    
+
     imports = [
       ../cli-fish.nix     # import module for fish cli shell
     ];
 
-    home.sessionVariables.SHELL = pkgs.fish;
+    home.sessionVariables.SHELL = "${pkgs.fish}/bin/fish";
     programs.home-manager.enable = true;
   }
