3dae54c050ebc82e4a3ec6e704401eae16cbb90e
Suzu <sou.suzumi@gmail.com>
2025-10-01 06:00:04 -0300

    Fix integrations in modules/base.nix

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -24,6 +24,7 @@
     users.users.suzu = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" ];
+      ignoreShellProgramCheck = true;
       shell = pkgs.fish;	# Defines fish as default user shell
     };
     security.sudo.wheelNeedsPassword = true;
