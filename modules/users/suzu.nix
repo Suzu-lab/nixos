8dcd91ec6a5c8b7a7f666abdb17dce83942a40b6
Suzu <sou.suzumi@gmail.com>
2025-10-02 21:47:44 -0300

    Minor error correction

--- modules/users/suzu.nix
+++ modules/users/suzu.nix
@@ -12,4 +12,7 @@
 
     home.sessionVariables.SHELL = "${pkgs.fish}/bin/fish";
     programs.home-manager.enable = true;
+
+    # Config to create backup of previous config files with home-manager
+    home-manager.backupFileExtension = "backup";
   }
