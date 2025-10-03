1748441b3ebdb123caf56a2ce8fab2ccea5845bf
Suzu <sou.suzumi@gmail.com>
2025-10-03 02:35:36 -0300

    Splitting Hyprland configs

--- modules/hyprland-suzu.nix
+++ modules/hyprland-suzu.nix
@@ -1,33 +1,7 @@
   # Declarative config files for the desktop environment
-  { pkgs, home-manager, ... }:
+  { pkgs, ... }:
   {
-    #configure Thunar plugins
-    programs.thunar = {
-    	enable = true;
-    	plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
-    };
 
-  	# Essential GUI apps
-  	environment.systemPackages = with pkgs; [
-      cliphist
-      grim
-      hyprlock
-      hyprpaper
-      kitty
-  		mako
-  		slurp
-  		waybar
-  		wl-clipboard
-  		xarchiver
-  	];
-
-  	# GUI fonts
-  	fonts.packages = with pkgs; [
-  		noto-fonts
-  		noto-fonts-cjk-sans
-  		noto-fonts-emoji
-  		font-awesome
-  	];
     # Hyprland config (~/.config/hypr/hyprland.conf)
     xdg.configFile."hypr/hyprland.conf".source = ./hyprland/hyprland.conf;
 
