45cfe5bd0b241c88425804d8ec5fdedaa3c5fd82
Suzu <sou.suzumi@gmail.com>
2025-10-04 20:48:08 -0300

    Refactoring and reorganizing

--- modules/home-manager/apps/thunar.nix
+++ modules/home-manager/apps/thunar.nix
@@ -1,6 +1,6 @@
   #configure Thunar and it's plugins
 	{ pkgs, ... }: {
-  	programs.thunar = {
+  	programs.xfce.thunar = {
    		enable = true;
    		plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
   	};
