a459acae7e3bb3ae35817329e08d7bc0cdccbd22
Suzu <sou.suzumi@gmail.com>
2025-10-05 18:04:51 -0300

    Adjusting screenshot tool

--- modules/apps/thunar.nix
+++ modules/apps/thunar.nix
@@ -2,6 +2,10 @@
 	{ pkgs, ... }: {
   	programs.thunar = {
    		enable = true;
-   		plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+   		plugins = with pkgs.xfce; [
+   			thunar-archive-plugin
+				thunar-media-tags-plugin
+   			thunar-volman
+   		];
   	};
   }
