b651439449e908270f4872726b5a16398e4c8bee
Suzu <sou.suzumi@gmail.com>
2025-10-04 22:28:23 -0300

    Refactoring and reorganizing

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -1,22 +1,10 @@
   # Module for setting and configuring the basic Hyprland desktop
   { pkgs, ... }:
   {
-    # Enables graphic server without X
-    services.xserver.enable = false;
-
   	# Enables Hyprland WM
   	wayland.windowManager.hyprland = {
   		enable = true;
   		xwayland.enable = true; # enable compatibility layer for x11 apps
-  	};
-
-  	# Global variables for forcing wayland wherever possible
-  	home.sessionVariables = {
-  		NIXOS_OZONE_WL = "1";								# Electron apps/Steam
-  		GDK_BACKEND = "wayland,x11";				# GTK apps
-  		QT_QPA_PLATFORM = "wayland;xcb";		# Qt apps
-  		SDL_VIDEODRIVER = "wayland,x11";		#SDL
-  		_JAVA_AWT_WM_NONREPARENTING = "1";	#Java/Swing
   	};
 
   	# Declarative config files for the desktop environment
