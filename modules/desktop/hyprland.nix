b292da4665b3ce82f40cf601ef40e38cd49ba552
Suzu <sou.suzumi@gmail.com>
2025-10-07 02:04:39 -0300

    Wlogout, cliphist and swappy as modules

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -51,15 +51,12 @@
  	    	######################################################
  	    	# Tools for Hyprland
  	    	######################################################
- 	    	cliphist
  	    	grim
  	    	hyprlock
  	    	hyprpicker
  	    	polkit_gnome
  	    	slurp
- 	    	swappy
  	    	wl-clipboard
- 	    	wlogout
  	    	xarchiver
  	    ];
 
@@ -67,19 +64,12 @@
   		# Hyprland config (~/.config/hypr/hyprland.conf)
   		xdg.configFile."hypr/hyprland.conf".source = ./dotfiles/hyprland.conf;
 
-  		# Waybar config (~/.config/waybar/config.jsonc)
-#  		xdg.configFile."waybar/config.jsonc".source = ./dotfiles/waybar.jsonc;
 
-  		# Config of waybar style sheet (~/.config/waybar/style.css)
-#  		xdg.configFile."waybar/style.css".source = ./dotfiles/waybar.css;
-
-  		# Hyprpaper config (~/.config/hypr/hyprpaper.conf)
-  		xdg.configFile."hypr/hyprpaper.conf".source = ./dotfiles/hyprpaper.conf;
-
-  		# Mako config (~/.config/mako/config)
-#  		xdg.configFile."mako/config".source = ./dotfiles/mako.conf;
-
-  		# Wofi config (~/.config/wofi/style.css)
-#  		xdg.configFile."wofi/style.css".source = ./dotfiles/wofi.css;
+			# Activate cliphist as a Home Manager module
+			programs.cliphist.enable = true;
+			# Activate swappy as a Home Manager module
+			programs.swappy.enable = true;
+			# Activate wlogout as a Home Manager module
+			programs.wlogout.enable = true;
 		};
   }
