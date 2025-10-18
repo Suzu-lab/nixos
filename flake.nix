560ec27137023af994e41c96977817562b77f0d3
Suzu <sou.suzumi@gmail.com>
2025-10-18 15:29:47 -0300

    Fixed Steam

--- flake.nix
+++ flake.nix
@@ -41,7 +41,14 @@
       	modules = [
       		./hosts/vm/default.nix
       	];
-    	};
+      };
+      yosai = nixpkgs.lib.nixosSystem {
+      	system = "x86_64-linux";
+      	specialArgs = { inherit inputs; };
+      	modules = [
+      		./hosts/yosai/default.nix
+      	];
+	  };
   	};
   };
 }
---
+++ hosts/yosai/default.nix
@@ -0,0 +1,29 @@
+  # Configuration file specific for this machine
+  { config, pkgs, inputs, ... }:
+  {
+    imports = [
+      # Import machine hardware config
+      ./hardware-configuration.nix
+
+			# Import system modules
+      ../../modules/nixos/base.nix		# default system module
+      ../../modules/nixos/hardware/audio.nix 	# pipewire module
+      ../../modules/nixos/services/desktop-essentials.nix 	# essential services for GUI
+      ../../modules/nixos/services/gayming.nix #module for setting up Steam and other gaming options
+      ../../modules/nixos/desktop/hyprland.nix
+      ../../modules/nixos/stylix.nix
+
+			# Importing system flakes modules
+			inputs.stylix.nixosModules.stylix
+			inputs.nurpkgs.modules.nixos.default
+
+			# Importing Home Manager module
+			inputs.home-manager.nixosModules.home-manager
+
+      # User config
+      ../../users/suzu/user.nix
+      ../../users/suzu/home.nix
+    ];
+
+		networking.hostName = "yosai";
+  }
